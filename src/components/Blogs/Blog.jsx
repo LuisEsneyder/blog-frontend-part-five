16c1de72b6050cf68d1b09531c85483d5e05c1b7
Luis Esneyder Fernández <luisexneider1999@gmail.com>
2023-05-07 16:14:23 -0500

    ejercicio 5.7

--- src/components/Blogs/Blog.jsx
+++ src/components/Blogs/Blog.jsx
@@ -1,4 +1,34 @@
+import { useState } from "react";
 const Blog = ({ blog }) => {
-  return <div>{blog.title}</div>;
+  const [visible, setVisible] = useState(false);
+  const hideBlogDetail = { display: visible ? "none" : "" };
+  const showBlogDetail = { display: visible ? "" : "none" };
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: "solid",
+    borderWdth: 1,
+    marginBottom: 5,
+  };
+  const togglableVisible = () => {
+    setVisible(!visible);
+  };
+  return (
+    <div style={blogStyle}>
+      <div style={hideBlogDetail}>
+        {blog.title}
+        <button onClick={togglableVisible}>view</button>
+      </div>
+      <div style={showBlogDetail}>
+        {blog.title}
+        <button onClick={togglableVisible}>hide</button>
+        <p>{blog.url}</p>
+        <p>
+          likes: {blog.likes} <button>like</button>
+        </p>
+        <p>{blog.author}</p>
+      </div>
+    </div>
+  );
 };
 export default Blog;
